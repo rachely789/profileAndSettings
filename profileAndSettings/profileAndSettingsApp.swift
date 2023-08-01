4e90f75ed9a958e0b3b6f9d6090eb0271e404aab
Rachel Yoon <rachelyoon789@gmail.com>
2023-08-01 14:06:18 -0700

    Initial Commit

---
+++ profileAndSettings/profileAndSettingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  profileAndSettingsApp.swift
+//  profileAndSettings
+//
+//  Created by Rachel Yoon on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct profileAndSettingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
